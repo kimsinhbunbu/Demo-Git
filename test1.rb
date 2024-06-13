5c6812f88c5a632a695838213574c9df9383b1f0
kimsinh20 <88784882+kimsinh20@users.noreply.github.com>
2024-06-13 09:20:51 +0700

    first commit

---
+++ test1.rb
@@ -0,0 +1,13 @@
+a = "sinh";
+x = 3
+if x ==2
+   puts "x is greater than 2"
+end
+
+for i in 0..5
+    if i > 2 then
+        break
+     end
+     puts "Value of local variable is #{i}"
+    end
+puts("hello words "+a);
---
+++ test2.rb
@@ -0,0 +1,12 @@
+class Sample
+  @name="sinh"
+  def hello
+     puts "Hello Ruby!"
+  end
+end
+
+# Now using above class to create objects
+object = Sample. new
+object.hello
+ex1 = Sample. new 
+ex1.hello
