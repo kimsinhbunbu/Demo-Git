6f1f288500345813d9191c8d0567d86821ff93f2
kimsinhbunbu <sinh.kim.phan@bunbu.vn>
2024-06-14 10:33:37 +0700

    change test1

--- test2.rb
+++ test2.rb
@@ -1,12 +1,30 @@
-class Sample
-  @name="sinh"
-  def hello
-     puts "Hello Ruby! "
-  end
-end
+# class Sample
+#   @name="sinh"
+#   def hello
+#      puts "Hello Ruby! "
+#   end
+# end
 
-# Now using above class to create objects
-object = Sample. new
-object.hello
-ex1 = Sample. new 
-ex1.hello
+# # Now using above class to create objects
+# object = Sample. new
+# object.hello
+# ex1 = Sample. new 
+# ex1.hello
+
+# class Customer
+#   @@no_of_customers = 0
+#   attr_accessor :cust_id, :cust_name, :cust_addr
+
+#   def initialize(id, name, addr)
+#     @cust_id = id
+#     @cust_name = name
+#     @cust_addr = addr
+#   end
+#   def sayGetting() 
+#     puts("hello");
+#   end
+# end
+
+# sinh = Customer.new(2, "sinh", "ha noi")
+# puts(sinh.cust_name)
+# sinh.sayGetting()
